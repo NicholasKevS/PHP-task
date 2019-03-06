38c7b9a8d6f83634bf37029d455743a57e1d90e0
NicholasKevS <nekoman.hunter.tech@gmail.com>
2019-03-06 20:58:20 +1100

    Added db settings and test connection

--- user_upload.php
+++ user_upload.php
@@ -1,1 +1,16 @@
 <?php
+
+//database default settings
+$host = "localhost";
+$username = "root";
+$password = "root_password";
+$dbName = "php-task";
+$tableName = "users";
+
+$con = mysqli_connect($host, $username, $password, $dbName);
+
+if(!$con) {
+	die("Connection failed: ".mysqli_connect_error());
+} else {
+	echo "Connected";
+}
