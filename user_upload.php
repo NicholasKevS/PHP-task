31ad63dde1749e2bc8a9d6a05da5ce5b6578644d
NicholasKevS <nekoman.hunter.tech@gmail.com>
2019-03-06 22:35:58 +1100

    Add shebang and implement user's name formalisation

--- user_upload.php
+++ user_upload.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 //script default settings
@@ -24,15 +25,26 @@ if(!$con) {
 $file = fopen($userFilePath, "r");
 
 if($file) {
-	fgetcsv($file); //read the first line but do nothing with it
+	fgetcsv($file); //read the first line and do nothing with it
 
 	while(!feof($file)) {
 		$user = fgetcsv($file);
 		if($user != array(null)) {
 			//print_r($user);
-			$insertQuery = "INSERT INTO $tableName (name, surname, email) VALUES ('{$user[0]}', '{$user[1]}', '{$user[2]}')";
-			if(!mysqli_query($con, $insertQuery)) {
-				echo "Error description: ".mysqli_error($con);
+			
+			//make first letter uppercase and add blackslashes to special character
+			$user[0] = addslashes(ucfirst(strtolower($user[0])));
+			$user[1] = addslashes(ucfirst(strtolower($user[1])));
+			
+			$valid = true;
+			
+			if($valid) {
+				$insertQuery = "INSERT INTO $tableName (name, surname, email) VALUES ('{$user[0]}', '{$user[1]}', '{$user[2]}')";
+				if(!mysqli_query($con, $insertQuery)) {
+					echo "Error message: ".mysqli_error($con);
+				}
+			} else {
+				echo "Error message: Your email {$user[2]} is not valid";
 			}
 		}
 	}
